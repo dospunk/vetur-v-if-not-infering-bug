75c51399f5794df2d368910024723daeaff6f03b
Connor Dooley <connor.dooley@azaleahealth.com>
2021-07-09 17:26:04 -0400

    show the error

--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    {{ fo }}
+  <div v-if="couldBeUndefined">
+    <span v-for="x in couldBeUndefined" :key="x">
+      {{ couldBeUndefined.length }}
+    </span>
   </div>
 </template>
 
-<script>
-import * as _ from 'lodash'
+<script lang="ts">
+import { defineComponent } from "vue"
 
-export default {
-  props: ['foo', 'bar']
-}
+
+export default defineComponent( {
+  props: {
+    foo: Boolean
+  },
+  computed: {
+    couldBeUndefined(): string[] | undefined {
+      if (this.foo) {
+        return ['thing', 'sing']
+        } else {
+          return undefined
+        }
+    }
+  }
+})
 </script>
